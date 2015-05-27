dd328843f5803e36f4807f1966f26b020779ff68
l3iggs <l3iggs@users.noreply.github.com>
2015-05-27 16:16:16 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+
+#LFS_LISTEN      # The address:port the server listens on, default: "tcp://:8080"
+#LFS_HOST        # The host used when the server generates URLs, default: "localhost:8080"
+#LFS_METADB      # The database file the server uses to store meta information, default: "lfs.db"
+#LFS_CONTENTPATH # The path where LFS files are store, default: "lfs-content"
+#LFS_ADMINUSER   # An administrator username, default: unset
+#LFS_ADMINPASS   # An administrator password, default: unset
+#LFS_CERT        # Certificate file for tls
+#LFS_KEY         # tls key
+#LFS_SCHEME      # set to 'https' to override default http
+
+LFS_LISTEN="tcp://:9999"
+LFS_HOST="127.0.0.1:9999"
+LFS_CONTENTPATH="content"
+LFS_ADMINUSER="l3iggs"
+LFS_ADMINPASS="sggi3l"
+#LFS_CERT="mine.crt"
+#LFS_KEY="mine.key"
+#LFS_SCHEME="https"
+
+export LFS_LISTEN LFS_HOST LFS_CONTENTPATH LFS_ADMINUSER LFS_ADMINPASS LFS_CERT LFS_KEY LFS_SCHEME
+
+./lfs-test-server
